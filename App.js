86527967d8f78e334db43aa9ef7dc4ce49d086fc
RodriC98374 <rodricesp.3.0@gmail.com>
2025-04-23 18:02:23 -0400

    feature: Creacion de los archivos y componentes necesarios y modificacion intregrandolos

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
 
 export default function App() {
+  const [showRegister, setShowRegister] = useState(false);
+
   return (
-    <View style={styles.container}>
-      <Text>Hola :)</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>      
+      <StatusBar style="dark" />
+      {showRegister ? (
+        <RegisterScreen onBack={() => setShowRegister(false)} />
+      ) : (
+        <LoginScreen onRegister={() => setShowRegister(true)} />
+      )}
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
